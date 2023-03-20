2b9ee9fd5f62241681fab7e50aa7183a988eb06f
Adjistyr <adjisetyaridwan7@gmail.com>
2023-03-20 15:59:11 +0700

    pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Adjisetya Ridwan orang";
+    }
+}
